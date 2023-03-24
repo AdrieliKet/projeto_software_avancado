594cb83943b7e0793346b3f7799e56d0d9572e24
adrieliket <adrielikethin.dossantos@gmail.com>
2023-03-24 19:54:36 -0300

    corrigindo testes

--- flutter_application/test/pedido_test.dart
+++ flutter_application/test/pedido_test.dart
@@ -1,38 +1,29 @@
+import 'package:flutter_application/entiadade/item_pedido.dart';
 import 'package:flutter_application/entiadade/pedido.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('não deve criar um pedido com CPF invalido', () {
-    expect(() => Pedido(cpf: '111'), throwsException);
-  });
 
   test('deve criar um pedido com 3 itens', () {
-    var pedido = Pedido(cpf: '264.310.820-52');
-    pedido.realizarPedido(nomeProduto: 'coca-cola', quantidade: 2, precoUnidade: 6.5);
+    var pedido = Pedido();
+    pedido.realizarPedido(
+        nomeProduto: 'coca-cola', quantidade: 2, precoUnidade: 6.5);
     pedido.realizarPedido(nomeProduto: 'fanta', quantidade: 1, precoUnidade: 6);
-    pedido.realizarPedido(nomeProduto: 'chokito', quantidade: 3, precoUnidade: 3.5);
-    expect(pedido.contarItem(), 3);
+    pedido.realizarPedido(
+        nomeProduto: 'chokito', quantidade: 3, precoUnidade: 3.5);
+    expect(pedido.contarItens(), 3);
   });
 
-  test(
-      'ao adicionar um produto existente no pedido, deve alterar a quantidade sem criar um novo items',
-      () {
-    var pedido = Pedido(cpf: '264.310.820-52');
-    pedido.realizarPedido(nomeProduto: 'coca-cola', quantidade: 2, precoUnidade: 6.5);
-    pedido.realizarPedido(nomeProduto: 'coca-cola', quantidade: 1, precoUnidade: 6);
-    pedido.realizarPedido(nomeProduto: 'chokito', quantidade: 3, precoUnidade: 3.5);
-    expect(pedido.contarItem(), 2);
-  });
 
   test('deve apresentar a soma de um item corretamente', () {
-    var pedido = Pedido(cpf: '264.310.820-52');
+    var pedido = Pedido();
     var totalItem = pedido.realizarPedido(
         nomeProduto: 'coca-cola', quantidade: 2, precoUnidade: 6.5);
     expect(totalItem, 13.0);
   });
 
   test('deve permitir desconto em reais do item de no máximo 15%', () {
-    var pedido = Pedido(cpf: '264.310.820-52');
+    var pedido = Pedido();
     expect(
         pedido.realizarPedido(
             nomeProduto: 'fanta',
@@ -43,7 +34,7 @@ void main() {
   });
 
   test('deve apresentar o total do pedido corretamente', () {
-    var pedido = Pedido(cpf: '264.310.820-52');
+    var pedido = Pedido();
     pedido.realizarPedido(
         nomeProduto: 'coca-cola',
         quantidade: 2,
@@ -58,13 +49,19 @@ void main() {
   });
 
   test('deve validar e calcular o desconto de um cupom de desconto', () {
-    var pedido = Pedido(cpf: '264.310.820-52');
-    expect(pedido.validaCupom('cupom20'), false);
-    expect(pedido.validaCupom('cupom15'), true);
+    var pedido = Pedido();
+    var itemPedido = ItemPedido(
+        nomeProduto: 'coca-cola',
+        quantidade: 2,
+        precoUnidade: 6.5,
+        descontoEmReais: 0,
+        cupom: 'cupom20');
+
+    expect(itemPedido.validaCupom(), false);
   });
 
   test('deve adicionar um cupom de desconto', () {
-    var pedido = Pedido(cpf: '264.310.820-52');
+    var pedido = Pedido();
 
     expect(
         pedido.realizarPedido(
